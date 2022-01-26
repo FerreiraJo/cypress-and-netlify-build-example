e7f88d013c5772934665c932ad1d8832a5b4f5a5
Jonathan Ferreira <jonathan@cloudwalk.io>
2022-01-26 19:13:42 -0300

    add cypress plugin

--- plugins/cypress/index.js
+++ plugins/cypress/index.js
@@ -1,5 +1,53 @@
+const cypress = require('cypress');
+
 module.exports = {
-    onPreBuild: () => {
-        console.log('Hello, world!');
+    onSuccess: async ({netlifyConfig}) => {
+        if (!netlifyConfig.build.environment.SITE_NAME !== 'app') {
+            console.log('Cypress skipped');      
+            return;    
+        }
+
+        const result =  cypress.run({
+            config: {
+                baseUrl: netlifyConfig.build.environemtn.DEPLOY_PRIME_URL,
+            },
+            spec: '**/*.{feature,features}',
+            env: {
+                access_token: process.env.CYPRESS_ACCESS_TOKEN
+            }
+        })
+
+        if(result.totalFailed){
+            const sha = utils.git.commits[0].sha;
+            const authorization = `token ${netlifyConfig.build.environment.NETLIFY_BOT_GITHUB_STATUS_TOKEN}`;
+            const deployURL = netlifyConfig.build.environment.DEPLOY_PRIME_URL
+        
+            const response = await fetch(
+                `https://api.github.com/repos/netlify/netlify-react-ui/statuses/${sha}`,
+                {
+                    method: 'POST',
+                    headers: {
+                        authorization,
+                    },
+                    body: JSON.stringify({
+                        status: 'failure',
+                        target_url: deployURL,
+                        context: 'cypress',
+                        message:'Cypress tests failed',
+                    }),
+                }
+            );
+        
+        
+            const data = await response.json();
+            const { status, statusText, ok } = response;
+        
+            if (!ok) {
+                const error = new Error(`${status} ${statusText}`);
+                error.data = data;
+                throw error;
+            }
+            return data;
+        }
     }
 };
